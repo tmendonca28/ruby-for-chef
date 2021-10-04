698a58df7ff3aa9ce3fd810c8c0f2cc93f969f83
Anthony Mendonca <17821357+tmendonca28@users.noreply.github.com>
2021-10-04 16:26:34 +0100

    Adds Control Flow in Ruby
    Signed-off-by: Anthony Mendonca <17821357+tmendonca28@users.noreply.github.com>

---
+++ Control Flow and Collections/control_flow.rb
@@ -0,0 +1,8 @@
+x = 10
+if x > 10
+    puts "x is greater than 22"
+elsif x < 10
+    puts "x is greater than 11"
+else
+    puts "x is equal to 10"
+end
---
+++ Control Flow and Collections/unless.rb
@@ -0,0 +1,7 @@
+playing = false
+# When interested in knowing if a condition is false
+unless playing
+    puts "Johnny is not playing"
+else
+    puts "Johnny is most definitely playing!"
+end
