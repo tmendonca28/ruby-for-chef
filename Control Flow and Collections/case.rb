ecfd28ac35b3c386d3ba365be7681863538daf46
Anthony Mendonca <17821357+tmendonca28@users.noreply.github.com>
2021-10-04 18:38:07 +0100

    Adds loop structures in Ruby
    Signed-off-by: Anthony Mendonca <17821357+tmendonca28@users.noreply.github.com>

---
+++ Control Flow and Collections/case.rb
@@ -0,0 +1,14 @@
+num = 3
+
+case num
+when 0
+    puts "Zero"
+when 1
+    puts "One"
+when 2
+    puts "Two"
+when 3
+    puts "Three"
+else
+    puts "Num is definitely greater than 3"
+end
---
+++ Control Flow and Collections/until.rb
@@ -0,0 +1,6 @@
+# Until works while the condition is false and stops when the condition is true
+x = 6
+until x > 10
+    puts x
+    x += 1
+end
