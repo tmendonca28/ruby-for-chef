c2092fa83e2add6c2c2e0cae7bd3be4fb6de972f
Anthony Mendonca <17821357+tmendonca28@users.noreply.github.com>
2021-10-04 16:26:11 +0100

    Adds basics of ruby
    Signed-off-by: Anthony Mendonca <17821357+tmendonca28@users.noreply.github.com>

---
+++ Ruby Basics/hello.rb
@@ -0,0 +1,6 @@
+# Prints Hello World to console
+=begin
+    This is a multiline
+    comment
+=end
+puts "Hello World"
---
+++ Ruby Basics/variables.rb
@@ -0,0 +1,29 @@
+my_var = 5
+puts my_var
+
+x = 5
+y = 15
+puts x + y
+
+# Variable data types
+name = "Anthony"
+age = 25
+puts "My name is #{name} and I am #{age} years old"
+
+# String concatenation
+string_uno = "Ola "
+string_dos = "Mundo"
+puts string_uno+string_dos
+
+my_special_string = "SGU Rage"
+# getting the length of the string
+puts my_special_string.length
+
+# reverse the string
+puts my_special_string.reverse
+
+# printing in lowercase
+puts my_special_string.downcase
+
+# printing in uppercase
+puts my_special_string.upcase
